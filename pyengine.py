4b6f75c0b576ba29959ea34a1d3910e94505b849
raypho <becher.raphael@gmx.de>
2025-08-19 17:29:32 +0200

    Update pyengine.py

--- pyengine.py
+++ pyengine.py
@@ -15,15 +15,29 @@ def add_object(name, sprite_path, x=0, y=0):
     obj = {"name": name,
            "sprite": sprite,
            "x": x,
-           "y": y
+           "y": y,
+           "script": None
            }
     objects.append(obj)
-add_object("spieler", "bilder/biene.png", 100, 100)
+
+def add_script(objnumber, scriptname):
+    objects[objnumber]["script"] = script
+ add_object("spieler", "bilder/biene.png", 100, 100)
+print(objects)
 "pygame"
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                objects[0]["x"] -= 10
+            if event.key == pygame.K_RIGHT:
+                objects[0]["x"] += 10
+            if event.key == pygame.K_UP:
+                objects[0]["y"] -=  10
+            if event.key == pygame.K_DOWN:
+                objects[0]["y"] +=  10
     screen.fill("purple")
     for obj in objects:
         screen.blit(obj["sprite"], (obj["x"], obj["y"]))
