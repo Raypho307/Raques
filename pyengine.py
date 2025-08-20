d674cabcdded10d71b19d45d5c867c8b90dde524
raypho <becher.raphael@gmx.de>
2025-08-20 16:31:32 +0200

    Added video support

--- pyengine.py
+++ pyengine.py
@@ -1,29 +1,38 @@
 import pygame
+from pygamevideo import Video
+
 
 "init pygame"
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Pyengine")
 running = True
+video = Video("video.mp4")
+video.play()
 
 "Variables"
 objects = []
 
 "Functions"
-def add_object(name, sprite_path, x=0, y=0):
+def add_object(name, sprite_path, script, x=0, y=0):
     sprite = pygame.image.load(sprite_path)
     obj = {"name": name,
            "sprite": sprite,
            "x": x,
            "y": y,
-           "script": None
+           "script": script
            }
     objects.append(obj)
-
-def add_script(objnumber, scriptname):
-    objects[objnumber]["script"] = script
- add_object("spieler", "bilder/biene.png", 100, 100)
+def move():
+    if event.key == pygame.K_w:
+        print("etst")
+add_object("spieler", "bilder/biene.png", move, 100, 100)
 print(objects)
+"run scripts"
+def run_script(name):
+    for obj in objects:
+        if obj["name"] == name and obj["script"] is not None:
+            obj["script"]()
 "pygame"
 while running:
     for event in pygame.event.get():
@@ -38,9 +47,12 @@ while running:
                 objects[0]["y"] -=  10
             if event.key == pygame.K_DOWN:
                 objects[0]["y"] +=  10
+            run_script("spieler")
     screen.fill("purple")
     for obj in objects:
         screen.blit(obj["sprite"], (obj["x"], obj["y"]))
+    video.draw_to(screen, (0, 0))
     pygame.display.flip()
+    
     
 pygame.quit()
