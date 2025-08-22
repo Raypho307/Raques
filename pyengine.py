0c2bcbd44b83ec9dce04c15b4b296f3a9c4f778b
raypho <becher.raphael@gmx.de>
2025-08-22 13:55:01 +0200

    Added simple collision detection and gravity

--- pyengine.py
+++ pyengine.py
@@ -1,5 +1,6 @@
 import pygame
 from pygamevideo import Video
+from time import sleep
 
 
 "init pygame"
@@ -8,10 +9,11 @@ screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Pyengine")
 running = True
 video = Video("video.mp4")
-video.play()
+#video.play()
 
 "Variables"
 objects = []
+objectswc = []
 
 "Functions"
 def add_object(name, sprite_path, script, x=0, y=0):
@@ -20,19 +22,31 @@ def add_object(name, sprite_path, script, x=0, y=0):
            "sprite": sprite,
            "x": x,
            "y": y,
-           "script": script
+           "script": script,
+           "gravity": 0
            }
     objects.append(obj)
 def move():
     if event.key == pygame.K_w:
         print("etst")
-add_object("spieler", "bilder/biene.png", move, 100, 100)
-print(objects)
 "run scripts"
 def run_script(name):
     for obj in objects:
         if obj["name"] == name and obj["script"] is not None:
             obj["script"]()
+def apply_phisics(objnumber, gravity):
+    objects[objnumber]["gravity"] = gravity
+def apply_collision(objnumber):
+    objtoappend = objects[objnumber]
+    objectswc.append(objtoappend)
+    objects.pop(objnumber)
+add_object("spieler", "bilder/biene.png", move, 100, 100)
+add_object("spieler1", "bilder/biene.png", move, 500, 100)
+apply_collision(1)
+#apply_phisics(0, 1)
+
+print(objects)
+print(objectswc)
 "pygame"
 while running:
     for event in pygame.event.get():
@@ -51,8 +65,17 @@ while running:
     screen.fill("purple")
     for obj in objects:
         screen.blit(obj["sprite"], (obj["x"], obj["y"]))
-    video.draw_to(screen, (0, 0))
+    for obj in objectswc:
+        screen.blit(obj["sprite"], (obj["x"], obj["y"]))
+    #video.draw_to(screen, (0, 0))
+    for obj in objects:
+        obj["y"] += obj["gravity"]
+    for obj in objectswc:
+        for obj1 in objects:
+            if obj["y"] == obj1["y"] and obj["x"] == obj1["x"]:
+                #print("test")
+                obj1["y"]
     pygame.display.flip()
     
-    
+    sleep(0.01)
 pygame.quit()
