1ca5c46d734d3470fe1ffaa34235cb7ba939ad26
raypho <becher.raphael@gmx.de>
2025-08-26 22:30:20 +0200

    Update pyengine.py

--- pyengine.py
+++ pyengine.py
@@ -23,7 +23,9 @@ def add_object(name, sprite_path, script, x=0, y=0):
            "x": x,
            "y": y,
            "script": script,
-           "gravity": 0
+           "gravity": 0,
+           "cry": 0,
+           "crx": 0
            }
     objects.append(obj)
 def move():
@@ -37,11 +39,13 @@ def run_script(name):
 def apply_phisics(objnumber, gravity):
     objects[objnumber]["gravity"] = gravity
 def apply_collision(objnumber):
+    objects[objnumber]["cry"] = 80
+    objects[objnumber]["crx"] = 60
     objtoappend = objects[objnumber]
     objectswc.append(objtoappend)
     objects.pop(objnumber)
 add_object("spieler", "bilder/biene.png", move, 100, 100)
-add_object("spieler1", "bilder/biene.png", move, 500, 100)
+add_object("spieler1", "bilder/biene.png", move, 200, 100)
 apply_collision(1)
 #apply_phisics(0, 1)
 
@@ -72,9 +76,14 @@ while running:
         obj["y"] += obj["gravity"]
     for obj in objectswc:
         for obj1 in objects:
-            if obj["y"] == obj1["y"] and obj["x"] == obj1["x"]:
-                #print("test")
-                obj1["y"]
+            if obj["y"] - obj["cry"] >= obj1["y"] and obj["x"] - obj1["x"] < obj["crx"] and obj["x"] - obj1["x"] > obj["crx"] - obj["crx"] *2 and obj["y"] + obj["cry"] /2 <= obj1["y"]:
+                obj1["y"] -= 1
+            if obj["x"] - obj["crx"] >= obj1["x"] and obj["y"] - obj1["y"] < obj["cry"] and obj["y"] - obj1["y"] > obj["cry"] - obj["cry"] *2 and obj["x"] + obj["crx"] /2 <= obj1["x"]:
+                obj1["x"] -= 1
+            if obj["y"] + obj["cry"] >= obj1["y"] and obj["x"] - obj1["x"] < obj["crx"] and obj["x"] - obj1["x"] > obj["crx"] - obj["crx"] *2 and obj["y"] - obj["cry"] /2 <= obj1["y"]:
+                obj1["y"] += 1
+            if obj["x"] + obj["crx"] >= obj1["x"] and obj["y"] - obj1["y"] < obj["cry"] and obj["y"] - obj1["y"] > obj["cry"] - obj["cry"] *2 and obj["x"] - obj["crx"] /2 <= obj1["x"]:
+                obj1["x"] += 1
     pygame.display.flip()
     
     sleep(0.01)
