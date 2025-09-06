006413e387bf41fb119eee32c82c676b9cde40f7
raypho <becher.raphael@gmx.de>
2025-09-06 15:27:45 +0200

    Mor cleaning up

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import pyengine
+from time import sleep
 print("test")
-pyengine.running = True
+
+pyengine.add_object("spieler", "bilder/biene.png", None, 100, 100)
 print(pyengine.running)
-#pyengine.add_object("spieler", "bilder/biene.png", None, 100, 100)
+pyengine.run()
+pyengine.objects[0]["y"] - 100
--- pyengine.py
+++ pyengine.py
@@ -4,13 +4,11 @@ from time import sleep
 import importlib.util
 import os
 
-running = False
-screen = pygame.display.set_mode((1280, 720))
 "init pygame"
-
 pygame.init()
+screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Pyengine")
-running = False
+running = True
 #video = Video("video.mp4")
 #video.play()
 "Variables"
@@ -50,34 +48,35 @@ def build_game():
 
 print(objects)
 print(objectswc)
-while running == False:
-    print("not running")
-    sleep(0.5)
 "pygame"
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill("purple")
-    for obj in objects:
-        screen.blit(obj["sprite"], (obj["x"], obj["y"]))
-    for obj in objectswc:
-        screen.blit(obj["sprite"], (obj["x"], obj["y"]))
-    #video.draw_to(screen, (0, 0))
-    for obj in objects:
-        obj["y"] += obj["gravity"]
-    for obj in objectswc:
-        for obj1 in objects:
-            if obj["y"] - obj["cry"] == obj1["y"] and obj["x"] - obj1["x"] < obj["crx"] and obj["x"] - obj1["x"] > obj["crx"] - obj["crx"] *2:
-                obj1["y"] -= 10
-            if obj["x"] - obj["crx"] == obj1["x"] and obj["y"] - obj1["y"] < obj["cry"] and obj["y"] - obj1["y"] > obj["cry"] - obj["cry"] *2:
-                obj1["x"] -= 10
-            if obj["y"] + obj["cry"] == obj1["y"] and obj["x"] - obj1["x"] < obj["crx"] and obj["x"] - obj1["x"] > obj["crx"] - obj["crx"] *2:
-                obj1["y"] += 10
-            if obj["x"] + obj["crx"] == obj1["x"] and obj["y"] - obj1["y"] < obj["cry"] and obj["y"] - obj1["y"] > obj["cry"] - obj["cry"] *2:
-                obj1["x"] += 10
-    pygame.display.flip()
-    for obj in objects:
-        if obj["script"] != None:
-            obj["script"]()
-    sleep(0.01)
+def run():
+    global running
+    while running:
+        #print("test")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            for obj in objects:
+                if obj["script"] != None:
+                    obj["script"]()
+        screen.fill("purple")
+        for obj in objects:
+            screen.blit(obj["sprite"], (obj["x"], obj["y"]))
+        for obj in objectswc:
+            screen.blit(obj["sprite"], (obj["x"], obj["y"]))
+        #video.draw_to(screen, (0, 0))
+        for obj in objects:
+            obj["y"] += obj["gravity"]
+        for obj in objectswc:
+            for obj1 in objects:
+                if obj["y"] - obj["cry"] == obj1["y"] and obj["x"] - obj1["x"] < obj["crx"] and obj["x"] - obj1["x"] > obj["crx"] - obj["crx"] *2:
+                    obj1["y"] -= 10
+                if obj["x"] - obj["crx"] == obj1["x"] and obj["y"] - obj1["y"] < obj["cry"] and obj["y"] - obj1["y"] > obj["cry"] - obj["cry"] *2:
+                    obj1["x"] -= 10
+                if obj["y"] + obj["cry"] == obj1["y"] and obj["x"] - obj1["x"] < obj["crx"] and obj["x"] - obj1["x"] > obj["crx"] - obj["crx"] *2:
+                    obj1["y"] += 10
+                if obj["x"] + obj["crx"] == obj1["x"] and obj["y"] - obj1["y"] < obj["cry"] and obj["y"] - obj1["y"] > obj["cry"] - obj["cry"] *2:
+                    obj1["x"] += 10
+        pygame.display.flip()
+        sleep(0.01)
+    pygame.quit()
